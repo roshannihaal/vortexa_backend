df972a53c97928cda20ef8340b6c550082e1fcd2
roshannihaal <roshan.nihaal18@gmail.com>
2024-01-27 14:31:37 +0530

    Added Prisma Created tables with prisma Added seed file

---
+++ prisma/games.ts
@@ -0,0 +1,18 @@
+export const games = [
+  {
+    id: 'FLAMES',
+    name: 'Flames',
+  },
+  {
+    id: 'TIC_TAC_TOE',
+    name: 'Tic Tac Toe',
+  },
+  {
+    id: 'ROCK_PAPER_SCISSORS',
+    name: 'Rock Paper Scissors',
+  },
+  {
+    id: 'ROLL_RUSH',
+    name: 'Roll Rush',
+  },
+]
